58c2f9bd42c83b46d8b00bfc9d996774e322dedb
iheartlearning <raphael@iheartlearning.net>
2017-02-19 13:05:01 -0500

    HW4 I changed this to reading a news file and getting keyterms

--- Homework4_HTML/HW4_HtmlParse.py
+++ Homework4_HTML/HW4_HtmlParse.py
@@ -4,26 +4,35 @@ from watson_developer_cloud import AlchemyLanguageV1
 import json
 from os.path import join, dirname
 
+
+""" This program fetches a news article from Yahoo! News, passes the text to the
+AlchemyLanuage API and retrieve the top 10 keywords by relevence:
+
+"""
+
+full_text = ""
+
 in_file = open(".apikey", "r")
 watson_api_key = in_file.read().splitlines()
 in_file.close()
 
-
 alchemy_language = AlchemyLanguageV1(api_key=watson_api_key)
-link = "https://twitter.com/realDonaldTrump"
-f = urllib.urlopen(link)
-website = f.read()
+link = "https://www.yahoo.com/news/advances-imaging-could-deepen-knowledge-brain-200758111.html"
+website = urllib.urlopen(link).read()
 
 soup =  BeautifulSoup(website, "html.parser")
-tweets =  soup('p', {'class': 'tweet-text'})
+article =  soup('p', {'class': 'canvas-atom'})
 
-print ( "Sentiment of the last 20 tweets from Donald Trump @theRealDonaldTrump:")
-print("")
+for paragraph in article:
+    full_text = full_text + '\n' + paragraph.contents[0]
 
-for tweet in tweets:
-    print "Tweet Text: " + tweet.contents[0]
-    response = alchemy_language.sentiment(text=tweet.contents[0], language='english', )
-    print "Sentiment: " + response["docSentiment"]["type"]
-    print ""
+response = alchemy_language.keywords(text=full_text,  max_keywords=10, )
 
+keywords = response["keywords"]
+
+print "Top 10 keywords from the article: "+ link + ":"
+print("key word:relevence")
+print("------------------")
+for word in keywords:
+    print word["text"] +":" + word["relevance"]
 
